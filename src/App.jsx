fc2c10fbfd62cd9c9534b03558a7bfacbd289b86
Yash Solanki <thisisyashs@gmail.com>
2024-08-09 18:42:10 +0530

    feature : Resize

--- src/App.jsx
+++ src/App.jsx
@@ -31,30 +31,129 @@ function createElement(id, x1, y1, x2, y2, tool) {
 }
 
 function getSelectableElement(clientX, clientY, elements) {
-  let selectedElement;
+  let selectableElement;
   elements.forEach((element) => {
     const { x1, y1, x2, y2, tool } = element;
+    //check if the mouse is close to the shape
     switch (tool) {
       case "line":
-        //check if the point is close to the line
         if (
           distance(x1, y1, clientX, clientY) +
             distance(x2, y2, clientX, clientY) -
             distance(x1, y1, x2, y2) <
           1
         ) {
-          selectedElement = element;
+          selectableElement = element;
         }
         break;
       case "rectangle":
-        // console.log(x, y, x1, x1 + x2, y1, y1 + y2);
         if (clientX >= x1 && clientX <= x2 && clientY >= y1 && clientY <= y2) {
-          selectedElement = element;
+          selectableElement = element;
         }
         break;
     }
+
+    //if the element is selectable, check if it mouse is closer to the corners
+    if (selectableElement) {
+      const { x1, y1, x2, y2 } = selectableElement;
+      const corners = [
+        { x: x1, y: y1, type: "tl" },
+        { x: x2, y: y1, type: "tr" },
+        { x: x2, y: y2, type: "br" },
+        { x: x1, y: y2, type: "bl" },
+      ];
+      let isCloseToCorner = false;
+      corners.forEach((corner) => {
+        if (distance(corner.x, corner.y, clientX, clientY) < 10) {
+          isCloseToCorner = true;
+          selectableElement.corner = corner;
+        }
+      });
+      if (!isCloseToCorner) {
+        selectableElement.corner = null;
+      }
+    }
   });
-  return selectedElement;
+  return selectableElement;
+}
+
+function adjustElement(element) {
+  const { x1, y1, x2, y2, tool } = element;
+  switch (tool) {
+    case "line":
+      if (x1 > x2) {
+        return {
+          x1: x2,
+          y1: y2,
+          x2: x1,
+          y2: y1,
+        };
+      } else {
+        return {
+          x1,
+          y1,
+          x2,
+          y2,
+        };
+      }
+    case "rectangle":
+      return {
+        x1: Math.min(x1, x2),
+        y1: Math.min(y1, y2),
+        x2: Math.max(x1, x2),
+        y2: Math.max(y1, y2),
+      };
+  }
+}
+
+function getCursorType(element) {
+  if (element.corner) {
+    switch (element.corner.type) {
+      case "tl":
+        return "nw-resize";
+      case "tr":
+        return "ne-resize";
+      case "br":
+        return "se-resize";
+      case "bl":
+        return "sw-resize";
+    }
+  } else {
+    return "move";
+  }
+}
+function resizedCoords(prevCoords, x, y, clientX, clientY, type) {
+  const { x1, y1, x2, y2 } = prevCoords;
+  switch (type) {
+    case "tl":
+      return {
+        x1: clientX,
+        y1: clientY,
+        x2,
+        y2,
+      };
+    case "tr":
+      return {
+        x1,
+        y1: clientY,
+        x2: clientX,
+        y2,
+      };
+    case "br":
+      return {
+        x1,
+        y1,
+        x2: clientX,
+        y2: clientY,
+      };
+    case "bl":
+      return {
+        x1: clientX,
+        y1,
+        x2,
+        y2: clientY,
+      };
+  }
 }
 
 function App() {
@@ -74,8 +173,8 @@ function App() {
     });
   }, [elements]);
 
-  function updateElement(id, x1, y1, x2, y2, type) {
-    const element = createElement(id, x1, y1, x2, y2, type);
+  function updateElement(id, x1, y1, x2, y2, tool) {
+    const element = createElement(id, x1, y1, x2, y2, tool);
     const updatedElements = elements.map((el) => (el.id === id ? element : el));
     setElements(updatedElements);
   }
@@ -89,7 +188,7 @@ function App() {
         element.offsetX = offsetX;
         element.offsetY = offsetY;
         setSelectedElement(element);
-        setAction("moving");
+        element.corner ? setAction("resizing") : setAction("moving");
       }
     } else {
       //Tools : line, rectangle
@@ -109,13 +208,12 @@ function App() {
   const handleMouseMove = (e) => {
     //change the cursor based on the tool
     if (tool === "selection") {
-      e.target.style.cursor = getSelectableElement(
-        e.clientX,
-        e.clientY,
-        elements
-      )
-        ? "move"
-        : "default";
+      const element = getSelectableElement(e.clientX, e.clientY, elements);
+      if (element) {
+        e.target.style.cursor = getCursorType(element);
+      } else {
+        e.target.style.cursor = "default";
+      }
     }
 
     //Handle action if not none
@@ -132,9 +230,31 @@ function App() {
       const { x1, y1 } = elements[elements.length - 1];
       const id = elements.length - 1;
       updateElement(id, x1, y1, clientX, clientY, tool);
+    } else if (action === "resizing") {
+      const { clientX, clientY } = e;
+      const { id, corner, tool, ...prevCoords } = selectedElement;
+      // console.log("coordinates", coordinates);
+      // console.log("corner", corner);
+      const { x, y, type } = corner;
+      const { x1, y1, x2, y2 } = resizedCoords(
+        prevCoords,
+        x,
+        y,
+        clientX,
+        clientY,
+        type
+      );
+      updateElement(id, x1, y1, x2, y2, tool);
     }
   };
   const handleMouseUp = () => {
+    const { id, tool } = elements[elements.length - 1];
+    //if the action is drawing, adjust the element's coords to have x1, y1 as the top-left corner and x2, y2 as the bottom-right corner
+    if (action === "drawing" || action === "resizing") {
+      const { x1, y1, x2, y2 } = adjustElement(elements[elements.length - 1]);
+      updateElement(id, x1, y1, x2, y2, tool);
+    }
+
     //reset the action and selected element
     setAction("none");
     setSelectedElement(null);
